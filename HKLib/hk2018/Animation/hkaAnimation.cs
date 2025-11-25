aff6556a9453c90bbe5dd898fbb9a45aed78027d
obilang <obilangwen@gmail.com>
2025-11-25 19:37:18 +0800

    Make the hkaAnimation partial class so we can keep the autogen class untouched

--- HKLib/hk2018/Animation/hkaAnimation.cs
+++ HKLib/hk2018/Animation/hkaAnimation.cs
@@ -1,36 +1,7 @@
 namespace HKLib.hk2018;
 
-public class hkaAnimation : hkReferencedObject
+public partial class hkaAnimation : hkReferencedObject
 {
-    public hkaAnimation.AnimationType m_type;
-
-    public float m_duration;
-
-    public int m_numberOfTransformTracks;
-
-    public int m_numberOfFloatTracks;
-
-    public hkaAnimatedReferenceFrame? m_extractedMotion;
-
-    public List<hkaAnnotationTrack> m_annotationTracks = new();
-
-    public struct TrackAnnotation
-    {
-        public ushort m_trackID;
-        public hkaAnnotationTrack.Annotation m_annotation;
-    }
-
-    public enum AnimationType : int
-    {
-        HK_UNKNOWN_ANIMATION = 0,
-        HK_INTERLEAVED_ANIMATION = 1,
-        HK_MIRRORED_ANIMATION = 2,
-        HK_SPLINE_COMPRESSED_ANIMATION = 3,
-        HK_QUANTIZED_COMPRESSED_ANIMATION = 4,
-        HK_PREDICTIVE_COMPRESSED_ANIMATION = 5,
-        HK_REFERENCE_POSE_ANIMATION = 6
-    }
-
     public AnimationType GetAnimationType()
     {
         return m_type;
